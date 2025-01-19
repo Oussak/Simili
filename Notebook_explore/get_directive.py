70b2ad49a4f8fa6266aef561dcdbf81ec999bb5b
Ouss Bendri <mr.oussama.bendriss@gmail.com>
2025-01-19 14:37:55 +0100

    Add get_directive script

--- Notebook_explore/get_directive.py
+++ Notebook_explore/get_directive.py
@@ -9,3 +9,21 @@ import requests
 import json
 
 
+#Step 1: get dossier legislatif
+def get_doss_legi(textId, date): # LEGITEXT000006075116 2021-04-15
+  url = 'https://sandbox-api.piste.gouv.fr/dila/legifrance/lf-engine-app/consult/legiPart'
+  headers = {'accept': 'application/json','Content-Type': 'application/json', 'Authorization': 'Bearer ' + access_token}
+  data = { #"searchedString":"constitution 1958",
+        "date": date,
+        "textId":textId }
+  response = requests.post(url= url, headers =headers, json= data )
+  return response.json()["dossiersLegislatifs"][0]['id']
+
+#Step 2: get list of directives
+
+#Step 3: get Celex of directive from JORF
+
+#Step 4: get text from Eurlex with Sparql in Cellar database 
+
+#Step 5: clean with Firecrawl
+
